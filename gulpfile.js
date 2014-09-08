9ff219c007bd9c7c5ed0ed25766f030a95111153
Andy Taylor <andytlr@gmail.com>
2014-09-08 11:50:32 +1000

    It generates the manifest but without folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var manifest = require('gulp-manifest');
 
 gulp.task('default', function () {
 
+});
+
+gulp.task('manifest', function(){
+  gulp.src([
+      '*.html',
+      'assets/*.js',
+      'assets/*.css',
+      'assets/*.png',
+      'vendor/*.js'
+    ])
+    .pipe(manifest({
+      hash: true,
+      timestamp: false,
+      network: ['http://*', 'https://*', '*'],
+      filename: 'homeslice-new.appcache'
+     }))
+    .pipe(gulp.dest(''));
 });

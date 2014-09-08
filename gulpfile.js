697938fbec28cba91d1dd710d9501b220a4ca9d5
Andy Taylor <andytlr@gmail.com>
2014-09-08 11:29:16 +1000

    Remove fingerprinting stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
-var gulp   = require('gulp');
-var rev    = require('gulp-rev');
+var gulp = require('gulp');
 
 gulp.task('default', function () {
-  return gulp.src(['assets/*.js', 'assets/*.css', 'assets/*.png'])
-    .pipe(rev())
-    .pipe(gulp.dest('dist'));
+
 });
